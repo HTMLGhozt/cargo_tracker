d576bfa5a1fdba93a3d8d02d336d1b3817a1b434
klau5 <jografie@gmail.com>
2019-12-28 17:20:54 +0530

    added Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+// TODO: validate/register through Firebase
+// TODO: onSubmit handler --> AdminPanel.js
+
 const Login = () => {
-  return <div>Login here</div>;
+  return (
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="validationDefault01"
+          aria-describedby="emailHelp"
+        />
+        <small id="emailHelp" class="form-text text-muted">
+          We'll never share your email with anyone else.
+        </small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="validationDefault02" />
+      </div>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default Login;
