7fa376772d2650991b0546a96608098db0490b1a
Thomas Dillard <Thomas.Dillard.studios@gmail.com>
2019-12-28 13:26:42 -0500

    Fixes routing navigation.
    Your NavLink is a styleable replacement for the a-tag when you're routing a SPA. 
    It formats to:
    ```html
    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
      <li class="nav-item">
        <a class="nav-link active" role="tab" href="/" aria-current="page">Home</a>
      </li>
      <li class="nav-item">
        <a class="nav-link" role="tab" href="/login">Profile</a>
      </li>
      <li class="nav-item">
        <a class="nav-link" role="tab" href="/contact">Contact</a>
      </li>
    </ul>
    ```

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,57 +3,47 @@ import { NavLink, Route } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Route>
-      <div>
-        <nav
-          className="navbar navbar-light bg-light"
-          style={{ margin: "10px" }}
-        >
-          <h1>io-cargo</h1>
-          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-            <li className="nav-item">
-              <a
-                className="nav-link active"
-                id="pills-home-tab"
-                data-toggle="pill"
-                href="#pills-home"
-                role="tab"
-                aria-controls="pills-home"
-                aria-selected="true"
-              >
-                <NavLink to="/">Home</NavLink>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                id="pills-profile-tab"
-                data-toggle="pill"
-                href="#pills-profile"
-                role="tab"
-                aria-controls="pills-profile"
-                aria-selected="false"
-              >
-                <NavLink to="/login">Profile</NavLink>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                id="pills-contact-tab"
-                data-toggle="pill"
-                href="#pills-contact"
-                role="tab"
-                aria-controls="pills-contact"
-                aria-selected="false"
-              >
-                <NavLink to="/contact">Contact</NavLink>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </Route>
+    <div>
+      <nav
+        className="navbar navbar-light bg-light"
+        style={{ margin: "10px" }}
+      >
+        <h1>io-cargo</h1>
+        <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+          <li className="nav-item">
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              role="tab"
+              to="/"
+              exact
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink 
+              activeClassName="active"
+              className="nav-link"
+              role="tab"
+              to="/login"
+            >
+              Profile
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              role="tab"
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
